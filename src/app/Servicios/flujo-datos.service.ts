e4a8d88c9eeaa3709540480a5de5a05eb36d5c74
bpbryalex98 <bpbryalex98@gmail.com>
2023-12-28 01:44:25 -0500

    Comunicacion Componentes. Actualizar Cliente Persona

--- src/app/Servicios/flujo-datos.service.ts
+++ src/app/Servicios/flujo-datos.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class FlujoDatosService {
 
+  private datosCompartidos: any;
+
   constructor() { }
+
+  setDatos(datos: any) {
+    this.datosCompartidos = datos;
+  }
+
+  getDatos() {
+    return this.datosCompartidos;
+  }
 }
